db3ea47e5352232720a293b83814a607256a281f
Jerry Kurian <jkurian770@gmail.com>
2018-06-24 00:20:56 -0400

    update mock data

--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"log"
-	"math/rand"
-	"strconv"
 	"encoding/json"
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 //Book struct (model)
 type Book struct {
-	ID	string `json:"id"`
-	Isbn	string `json:"isbn"`
-	Title	string `json:"title"`
-	Author	*Author `json:"author"`
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
 }
 
 //Author struct
 type Author struct {
-	Firstname	string `json:"firstname"`
-	Lastname	string `json:"lastname"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
 
 // Init books var as slice book struct
@@ -30,7 +28,7 @@ var books []Book
 //Handlers
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books);
+	json.NewEncoder(w).Encode(books)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
@@ -49,11 +47,11 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 
 }
 func main() {
-	r := mux.NewRouter();
+	r := mux.NewRouter()
 
 	//Mock data @todo - implement DB
-	books = append(books, Book{ID: "1", Isbn: "12345", Title: "Harry Potter and the Goblet of Fire", Author: &Author{Firstname: "Joanne", Lastname: "Rowling"})
-	books = append(books, Book{ID: "2", Isbn: "44382", Title: "Macbeth", Author: &Author{Firstname: "William", Lastname: "Shakespeare"})
+	books = append(books, Book{ID: "1", Isbn: "12345", Title: "Harry Potter and the Goblet of Fire", Author: &Author{Firstname: "Joanne", Lastname: "Rowling"}})
+	books = append(books, Book{ID: "2", Isbn: "44382", Title: "Macbeth", Author: &Author{Firstname: "William", Lastname: "Shakespeare"}})
 
 	//Route hanlders / End points
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
